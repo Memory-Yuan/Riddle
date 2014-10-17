7217c9104d90d8c9da7198b4fdadbba3d3a5c873
Memory Yuan <memory78078@gmail.com>
2014-10-17 18:00:35 +0800

    set config production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,14 @@ Rails.application.configure do
   # and use secure cookies. 
   config.force_ssl  =  true 
 
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address:              'smtp.gmail.com',
+        port:                 587,
+        domain:               'gmail.com',
+        user_name:            ENV["EMAIL"],
+        password:             ENV["PASSWORD"],
+        authentication:       'plain',
+        enable_starttls_auto: true
+    }
 end
